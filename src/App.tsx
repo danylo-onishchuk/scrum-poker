1aca7afc65bbbdb073ea34879e3527e40ac3794c
Danylo Onishchuk <danylo.o@mate.academy>
2022-07-17 15:26:54 +0300

    change endpoint

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { BasicTable } from './components/Table/Table';
 import { VoteForm } from './components/VoteForm/VoteForm';
 import { WSEvents } from './components/wsEvents';
 
-const client = new W3CWebSocket('wss://danylo-scrum-poker.herokuapp.com:9000');
+const client = new W3CWebSocket('wss://danylo-scrum-poker.herokuapp.com:32807');
 
 export function App() {
   const [clients, setClients] = useState([]);
